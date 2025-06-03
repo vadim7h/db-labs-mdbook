d1c0f0bd032112f35b38f22d790699269dd33b02
Vadim <malush.vadim@lll.kpi.ua>
2025-06-03 11:45:46 +0300

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const userRoutes = require('./routes/usersRoutes');
+
+app.use(express.json());
+app.use('/users', userRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
