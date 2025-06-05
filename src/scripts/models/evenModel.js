2118e849631f34e69275c00d17b206d65bcc8043
Vadim <malush.vadim@lll.kpi.ua>
2025-06-05 10:34:55 +0300

    Create evenModel.js

---
+++ src/scripts/models/evenModel.js
@@ -0,0 +1,23 @@
+const pool = require('../config/db');
+
+module.exports = {
+  async createEvent({ user_id, role_id, action }) {
+    const res = await pool.query(
+      'INSERT INTO "Event" (user_id, role_id, action) VALUES ($1, $2, $3) RETURNING *',
+      [user_id, role_id, action]
+    );
+    return res.rows[0];
+  },
+
+  async deleteEvent(id) {
+    await pool.query('DELETE FROM "Event" WHERE id = $1', [id]);
+  },
+
+  async updateEventAction(id, action) {
+    const res = await pool.query(
+      'UPDATE "Event" SET action = $1 WHERE id = $2 RETURNING *',
+      [action, id]
+    );
+    return res.rows[0];
+  }
+};
