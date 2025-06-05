ba5484908bedd159cd8557c61746e179535b1caf
Vadim <malush.vadim@lll.kpi.ua>
2025-06-05 12:56:02 +0300

    Update projectRoutes.js

--- src/scripts/routes/projectRoutes.js
+++ src/scripts/routes/projectRoutes.js
@@ -3,10 +3,9 @@ const router = express.Router();
 const projectController = require('../controllers/projectController');
 
 router.get('/', projectController.getAll);
-router.get('/user/:userId', projectController.getByUser);
+router.get('/user/:user_id', projectController.getByUserId);
 router.post('/', projectController.create);
 router.put('/:id', projectController.update);
 router.delete('/:id', projectController.remove);
-router.post('/add-user', projectController.addUser);
 
 module.exports = router;
