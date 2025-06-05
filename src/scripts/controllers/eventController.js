163b63f0a1dd34078f909685ad952a3d9c7554ae
Vadim <malush.vadim@lll.kpi.ua>
2025-06-05 13:21:43 +0300

    Update eventController.js

--- src/scripts/controllers/eventController.js
+++ src/scripts/controllers/eventController.js
@@ -10,19 +10,19 @@ module.exports = {
     }
   },
 
-  async remove(req, res) {
+  async update(req, res) {
     try {
-      await Event.deleteEvent(req.params.id);
-      res.json({ message: `Подію ${req.params.id} видалено` });
+      const event = await Event.updateEvent(req.params.id, req.body.action);
+      res.json(event);
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
   },
 
-  async update(req, res) {
+  async remove(req, res) {
     try {
-      const event = await Event.updateEvent(req.params.id, req.body.action);
-      res.json(event);
+      await Event.deleteEvent(req.params.id);
+      res.json({ message: `Подію ${req.params.id} видалено` });
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
