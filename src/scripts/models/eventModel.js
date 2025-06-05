3e63fccfbc31a649416436f802a42a4c700894ab
Vadim <malush.vadim@lll.kpi.ua>
2025-06-05 12:33:36 +0300

    Update eventModel.js

--- src/scripts/models/eventModel.js
+++ src/scripts/models/eventModel.js
@@ -1,10 +1,10 @@
 const pool = require('../config/db');
 
 module.exports = {
-  async createEvent({ user_id, role_id, action }) {
+  async createEvent({ user_id, action }) {
     const res = await pool.query(
-      'INSERT INTO "Event" (user_id, role_id, action) VALUES ($1, $2, $3) RETURNING *',
-      [user_id, role_id, action]
+      'INSERT INTO "Event" (user_id, action) VALUES ($1, $2) RETURNING *',
+      [user_id, action]
     );
     return res.rows[0];
   },
@@ -13,7 +13,7 @@ module.exports = {
     await pool.query('DELETE FROM "Event" WHERE id = $1', [id]);
   },
 
-  async updateEventAction(id, action) {
+  async updateEvent(id, action) {
     const res = await pool.query(
       'UPDATE "Event" SET action = $1 WHERE id = $2 RETURNING *',
       [action, id]
