10133d20dca4245af06fa5a5f52b1aafb4ea8caa
Vadim <malush.vadim@lll.kpi.ua>
2025-06-05 10:41:27 +0300

    Update app.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
-const userRoutes = require('./routes/usersRoutes');
 
 app.use(express.json());
+
+const userRoutes = require('./routes/usersRoutes');
+const eventRoutes = require('./routes/eventRoutes');
+const projectRoutes = require('./routes/projectRoutes');
 app.use('/users', userRoutes);
+app.use('/events', eventRoutes);
+app.use('/projects', projectRoutes);
 
 const PORT = 7000;
 app.listen(PORT, () => {
