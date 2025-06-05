9681437e685c2bf885e66c1c227110e1f1d5b626
Vadim <malush.vadim@lll.kpi.ua>
2025-06-05 10:38:00 +0300

    Update usersRoutes.js

--- src/scripts/routes/usersRoutes.js
+++ src/scripts/routes/usersRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/usersController');
+const userController = require('../controllers/userController');
 
-router.get('/', userController.getUsers);
-router.get('/:id', userController.getUser);
-router.post('/', userController.createUser);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.get('/', userController.getAll);
+router.get('/:id', userController.getById);
+router.post('/', userController.create);
+router.delete('/:id', userController.remove);
 
 module.exports = router;
